58731c5944c3a7c6e19e60f93ec3798602973e06
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-09-19 15:42:02 -0700

    Add files via upload

---
+++ ConvertArrayList.java
@@ -0,0 +1,38 @@
+
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class ConvertArrayList {
+
+    ArrayList<String> stringArrayList = new ArrayList<>();
+    private static final String COMMA_DELIMITER = ",";
+
+    public  void creatArray()
+
+    {
+        Scanner scanner = null;
+        try {
+            //Get the scanner instance
+            scanner = new Scanner(new File("C:\\Users\\postelnicu\\IdeaProjects\\FlorinJavaProject\\src\\JavaFlorin01\\src\\listofwordsj.csv"));
+            //Use Delimiter as COMMA
+            scanner.useDelimiter(COMMA_DELIMITER);
+            while(scanner.hasNext())
+            {
+                stringArrayList.add(scanner.next().strip());
+            }
+        }
+        catch (FileNotFoundException fe)
+        {
+            fe.printStackTrace();
+        }
+        finally
+
+        {
+            scanner.close();
+        }
+    }
+}
+
